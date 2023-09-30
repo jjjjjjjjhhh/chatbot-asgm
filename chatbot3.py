d808e6da8f5b0ea9a5da2b812aa3cfcfa8b53cb8
jjjjjjjjhhh <eejh-wp21@student.tarc.edu.my>
2023-10-01 01:53:21 +0800

    a

--- chatbot3.py
+++ chatbot3.py
@@ -14,10 +14,14 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import ConversationalRetrievalChain
 import tempfile
 import uuid
+from pdf2image import convert_from_path
+import pytesseract
+from PIL import Image
+import subprocess
 
-
-os.environ['REPLICATE_API_TOKEN'] = "r8_KQMuoOovvSTQ1V221paM9i3XtiPblOC03vJ7d"
-pinecone.init(api_key='dbc2c5be-9964-4d58-955b-e0025a21cd77', environment='gcp-starter')
+os.environ['REPLICATE_API_TOKEN'] = "r8_61ZBBa1uv7QmgSOZSzAn1KpWM5wSGq40w1krB"
+pinecone.init(api_key='42c2d8e4-e167-4087-8723-1b8701369251', environment='gcp-starter')
+#pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
 # App title
 st.set_page_config(page_title="Lao Gan Ma Chatbot 👩‍🍳🧉", page_icon="img/lgm.png")
@@ -56,107 +60,131 @@ for message in st.session_state.messages:
         with st.chat_message(message["role"], avatar='👩‍🍳'):
             st.write(message["content"])
     else:
-        with st.chat_message(message["role"], avatar='👩‍🎓'):  
+        with st.chat_message(message["role"], avatar='👩‍🎓'):
             st.write(message["content"])
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Hi, how can I help you?"}]
+
 st.sidebar.button('Clear Chat', on_click=clear_chat_history)
 
-
 def generate_llama2_response(prompt_input):
-    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    string_dialogue = ""
     for dict_message in st.session_state.messages:
+        content = str(dict_message["content"])
         if dict_message["role"] == "user":
-            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+            string_dialogue += "User: " + content + "\n\n"
         else:
-            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+            string_dialogue += "Assistant: " + content + "\n\n"
+    
+    # Call the replicate API
     output = replicate.run(llm, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
-                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
-    return output
+                                  "temperature": temperature, "top_p": top_p, "max_length": max_length, "repetition_penalty": 1})
+    
+    
+    if hasattr(output, '__iter__'):
+        response_content = ''.join(output)
+    else:
+        response_content = output
+    
+    return response_content
+
+def extract_text_from_pdf(pdf_path):
+    # Create a temporary directory to store the converted images
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Convert PDF to images using pdftoppm
+        subprocess.run(["bin/pdftoppm.exe", "-jpeg", pdf_path, os.path.join(temp_dir, "page")])
+
+        # Get the list of generated images
+        image_files = [os.path.join(temp_dir, image_file) for image_file in os.listdir(temp_dir)]
+        image_files.sort()  # Ensure pages are in order
+
+        # Extract text from each image
+        texts = []
+        for image_file in image_files:
+            with Image.open(image_file) as image:
+                text = pytesseract.image_to_string(image)
+                texts.append(text)
+
+        return "\n".join(texts)
+
+# PDF handling
+
+qa_chain = None
+index_name = "chatbot-1"
 
 
-# User-provided prompt
-if prompt := st.chat_input():
+if uploaded_file and (not hasattr(st.session_state, 'processed_file_name') or uploaded_file.name != st.session_state.processed_file_name):
+    
+    with st.spinner("Lao Gan Ma is reading your PDF..."):
+        if index_name in pinecone.list_indexes():
+            pinecone.delete_index(index_name)
+        pinecone.create_index(index_name, dimension=768, metric="cosine", pods=4, pod_type="s1.x1")
+        uploaded_file_name = uploaded_file.name
+        temp_dir = tempfile.TemporaryDirectory()
+        temp_file_path = os.path.join(temp_dir.name, uploaded_file_name)
+        with open(temp_file_path, "wb") as temp_file:
+            temp_file.write(uploaded_file.read())
+        loader = PyPDFLoader(temp_file_path)
+        documents = loader.load()
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+        texts = text_splitter.split_documents(documents)
+        if not any(txt for txt in texts if str(txt).strip()):
+            # If no text was extracted, it might be a scanned PDF. Use OCR.
+            extracted_text = extract_text_from_pdf(temp_file_path)
+            st.write("Extracted text from scanned PDF:\n", extracted_text)
+            texts = extracted_text
+        #st.write("Extracted text from PDF:\n", texts)
+        embeddings = HuggingFaceEmbeddings()
+        index = pinecone.Index(index_name)
+        vectordb = Pinecone.from_documents(texts, embeddings, index_name=index_name)
+        llm_model = Replicate(
+            model=llm,
+            model_kwargs={"temperature": temperature, "max_length": max_length, "top_p": top_p}
+        )
+        qa_chain = ConversationalRetrievalChain.from_llm(
+            llm_model,
+            vectordb.as_retriever(search_kwargs={'k': 2}),
+            return_source_documents=True
+        )
+    
+    st.session_state.processed_file_name = uploaded_file.name
+    st.session_state.texts = texts
+    st.session_state.pdf_processed = True
+    st.session_state.qa_chain = qa_chain
+
+
+elif hasattr(st.session_state, 'pdf_processed') and st.session_state.pdf_processed:
+    qa_chain = st.session_state.qa_chain
+
+def filter_response(response):
+    undesired_keywords = ["Note:", "User:"]
+    sentences = response.split('.')
+    filtered_sentences = [sent for sent in sentences if not any(keyword in sent for keyword in undesired_keywords)]
+    return '.'.join(filtered_sentences).strip()
+
+# User input handling
+chat_input_key = "pdf_query" if uploaded_file else "general_query"
+if prompt := st.chat_input(key=chat_input_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user", avatar='👩‍🎓'):
         st.write(prompt)
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant", avatar='👩‍🍳'):
-        with st.spinner("Thinking..."):
-            response = generate_llama2_response(prompt)
-            placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
-            placeholder.markdown(full_response)
-    message = {"role": "assistant", "content": full_response}
+    if uploaded_file and qa_chain:
+        with st.spinner("Generating Content..."):
+            result = qa_chain({'question': prompt, 'chat_history': []})
+            response_content = result['answer']
+    else:
+        with st.spinner("Generating Content..."):
+            response_content = generate_llama2_response(prompt)
+
+    response_content = filter_response(response_content)
+
+    message = {"role": "assistant", "content": response_content}
     st.session_state.messages.append(message)
-
-
-# pdf thingy (hvnt connected)
-
-if uploaded_file is not None:
-    uploaded_file_name = uploaded_file.name  # Get the name of the uploaded file
-
-    # Create a temporary directory to save the uploaded file
-    temp_dir = tempfile.TemporaryDirectory()
-    temp_file_path = os.path.join(temp_dir.name, uploaded_file_name)
-
-    # Save the uploaded file to the temporary directory
-    with open(temp_file_path, "wb") as temp_file:
-        temp_file.write(uploaded_file.read())
-    loader = PyPDFLoader(temp_file_path)
-    documents = loader.load()
-
-    # Split the documents into smaller chunks for processing
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.split_documents(documents)
-
-    embeddings = HuggingFaceEmbeddings()
-
-    index_name = "chatbot-1"
-    index = pinecone.Index(index_name)
-    vectordb = Pinecone.from_documents(texts, embeddings, index_name=index_name)
-
-    llm_model = Replicate(
-    model=llm,
-    model_kwargs={"temperature": temperature, "max_length": max_length, "top_p": top_p}
-    )
-
-    qa_chain = ConversationalRetrievalChain.from_llm(
-    llm_model,
-    vectordb.as_retriever(search_kwargs={'k': 2}),
-    return_source_documents=True
-    )
-
-   
-    if prompt := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user", avatar='👩‍🎓'):
-            st.write(prompt)
-
-            
-
-    if st.session_state.messages[-1]["role"] != "assistant":
-        with st.chat_message("assistant", avatar='👩‍🍳'):
-            with st.spinner("Thinking..."):
-                response = qa_chain.answer(prompt)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
-                    placeholder.markdown(full_response)
-                placeholder.markdown(full_response)
-        message = {"role": "assistant", "content": full_response}
-        st.session_state.messages.append(message)
-
-
-
-        
+    with st.chat_message("assistant", avatar='👩‍🍳'):
+        with st.spinner("Generating Content..."):
+            st.write(response_content)
 
 
